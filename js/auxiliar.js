c44bf15a8688a1bc42e1da6c62d4ee55780e499d
Iván Arriola <ivan.arriola@fing.edu.uy>
2022-09-26 20:42:05 -0300

    linea 80, cambie un dato.genres por un genres que se destructura en linea 58

--- js/auxiliar.js
+++ js/auxiliar.js
@@ -1,71 +1,62 @@
 //--------------Constantes------------------------------
-const URL = 'https://japceibal.github.io/japflix_api/movies-data.json';
-
-
+const URL = "https://japceibal.github.io/japflix_api/movies-data.json";
 
 //--------------Funciones Auxiliares------------------------
 
 //  Hace GET, obtiene un JSON y devuelve un objeto
 async function getData(url) {
-    return fetch(url)
-  .then(response => response.json())
+  return fetch(url).then((response) => response.json());
 }
 
-function filterGender(gender, input){
+function filterGender(gender, input) {
   //gender === array of objects [{}]
   let genderFound = false;
-  for (let i = 0; i < gender.length; i++){
-    genderFound = gender[i].name.toLowerCase().indexOf(input.toLowerCase()) > -1;
+  for (let i = 0; i < gender.length; i++) {
+    genderFound =
+      gender[i].name.toLowerCase().indexOf(input.toLowerCase()) > -1;
     // if encuentra al menos un genero que contiene el input, devolvemos genderFound y paramos el for
     //if (genderFound) break;
-
   }
   return genderFound;
 }
 
-function filtrar(datos, input){
-  return datos.filter(function(dato){
+function filtrar(datos, input) {
+  return datos.filter(function (dato) {
     const { title, genres, tagline, overview } = dato;
-    return title.toLowerCase().indexOf(input.toLowerCase()) > -1 ||
-    filterGender(genres, input) ||
-    tagline.toLowerCase().indexOf(input.toLowerCase()) > -1 || 
-    overview.toLowerCase().indexOf(input.toLowerCase()) > -1;
+    return (
+      title.toLowerCase().indexOf(input.toLowerCase()) > -1 ||
+      filterGender(genres, input) ||
+      tagline.toLowerCase().indexOf(input.toLowerCase()) > -1 ||
+      overview.toLowerCase().indexOf(input.toLowerCase()) > -1
+    );
   });
 }
 
 function cantidadEstrellas(amount) {
-  let stars = '';
+  let stars = "";
   let starsValue = Math.round(amount / 2);
-  
-  for (let i = 0; i < 5; i++) {
-      if (i < starsValue){
-        stars += '<span class="fa fa-star checked"></span>';
-      };
 
-      if (i >= starsValue){
-        stars += '<span class="fa fa-star color"></span>';
-      }
-  } 
+  for (let i = 0; i < 5; i++) {
+    stars += `<span class="fa fa-star ${i < starsValue ? `checked`: ``}"></span>`;
+  }
   return stars;
 }
 
-function recorrerGenres(array){
+function recorrerGenres(array) {
   let result = "";
-  for (const gender of array){
+  for (const gender of array) {
     result += gender.name + ", ";
   }
   // le cortamos los ultimos 2 caracteres ", "
-  result = result.slice(0, result.length -2);
+  result = result.slice(0, result.length - 2);
   return result;
 }
 
-function mostrarResultados(datos){
- 
-  let indexhtml = '';
-  datos.forEach(dato => {
-  let offCanvas = "offcanvasTop";
-  const { title, tagline, vote_average, overview, id } = dato;
-  indexhtml += ` 
+function mostrarResultados(datos) {
+  let indexhtml = "";
+  datos.forEach((dato) => {
+    const { title, tagline, vote_average, overview, id, release_date, revenue, runtime, genres } = dato;
+    indexhtml += ` 
     <li>
     <div class="box">
       <div class="result row" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop${id}" aria-controls="offcanvasTop">
@@ -86,7 +77,7 @@ function mostrarResultados(datos){
           ${overview}
           </div>
           <br>
-          <h6 class="color">${recorrerGenres(dato.genres)}</h6>
+          <h6 class="color">${recorrerGenres(genres)}</h6>
         </div>
         <div class="dropdown mt-3" id="boton">
         <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
@@ -103,5 +94,5 @@ function mostrarResultados(datos){
     </li>`;
   });
 
-lista.innerHTML = indexhtml;
+  lista.innerHTML = indexhtml;
 }
